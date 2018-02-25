d5097873a0bd01c3bb3c9a0dfacb6691df62d83d
Vitor Cavalcanti <vitorluizc@outlook.com>
2018-02-25 12:40:07 -0300

    Pages & Routing: Add products links and show ID on product route

---
+++ Pages & Routing/pages/products/_pid.vue
@@ -0,0 +1,13 @@
+<template>
+  <AppContainer>
+    <h1>Product ID: {{ $route.params.pid }}</h1>
+  </AppContainer>
+</template>
+
+<script>
+import AppContainer from '~/components/AppContainer'
+
+export default {
+  components: { AppContainer }
+}
+</script>
---
+++ Pages & Routing/pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppContainer>
+    <nuxt-link to="/products/1" class="link">Product 1</nuxt-link>
+    <nuxt-link to="/products/2" class="link">Product 2</nuxt-link>
+  </AppContainer>
+</template>
+
+<script>
+import AppContainer from '~/components/AppContainer'
+
+export default {
+  components: { AppContainer }
+}
+</script>
+
+<style>
+.link
+{
+  margin: 20px 0;
+}
+</style>
