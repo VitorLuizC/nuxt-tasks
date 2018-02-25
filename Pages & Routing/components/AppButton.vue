b3dd95101184d1a37ca04fa3823173c46e0d530c
Vitor Cavalcanti <vitorluizc@outlook.com>
2018-02-25 12:28:26 -0300

    Pages & Routing: Add AppButton & AppContainer components

---
+++ Pages & Routing/components/AppButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="AppButton">
+    <slot />
+  </button>
+</template>
+
+<style>
+.AppButton
+{
+  margin-top: 20px;
+  padding: 20px;
+  border: 0;
+  font-size: 20px;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  color: #fff;
+  background-color: coral;
+  cursor: pointer;
+}
+
+.AppButton::first-letter
+{
+  font-size: 25px;
+}
+
+.AppButton:hover,
+.AppButton:active
+{
+  background-color: rgb(247, 101, 49);
+}
+</style>
---
+++ Pages & Routing/components/AppContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="AppContainer">
+    <slot />
+  </section>
+</template>
+
+<style>
+.AppContainer
+{
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+</style>
